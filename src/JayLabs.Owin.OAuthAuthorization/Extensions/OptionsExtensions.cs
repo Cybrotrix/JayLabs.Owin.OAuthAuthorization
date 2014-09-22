5c59bd2003cdbfb961793642a966c28accfb527f
perokvist <per_okvist@live.se>
2014-09-22 13:28:48 +0200

    Added extensions for IJwtOptions

---
+++ src/JayLabs.Owin.OAuthAuthorization/Extensions/OptionsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JayLabs.Owin.OAuthAuthorization.Tokens;
+using Microsoft.Owin.Security.Jwt;
+
+namespace JayLabs.Owin.OAuthAuthorization.Extensions
+{
+
+    public static class OptionsExtensions
+    {
+       public static IIssuerSecurityTokenProvider CreateKeyIssuerSecurityTokenProvider(this IJwtOptions options)
+       {
+           var symmetricKeyBytes = Encoding.UTF8.GetBytes(options.JwtSigningKeyAsUtf8);
+           var symmetricKeyAsBase64 = Convert.ToBase64String(symmetricKeyBytes);
+
+           return new SymmetricKeyIssuerSecurityTokenProvider(
+               options.Issuer, symmetricKeyAsBase64);
+       }
+
+       public static JwtBearerAuthenticationOptions CreateJwtBearerAuthenticationOptions(this IJwtOptions options)
+       {
+           var providers = new[]
+                            {
+                                options.CreateKeyIssuerSecurityTokenProvider()
+                            };
+
+           return options.CreateJwtBearerAuthenticationOptions(jwtOptions => providers);
+       }
+
+       public static JwtBearerAuthenticationOptions CreateJwtBearerAuthenticationOptions(this IJwtOptions options, Func<IJwtOptions, IEnumerable<IIssuerSecurityTokenProvider>> providersFunc)
+       {
+            return new JwtBearerAuthenticationOptions
+            {
+                AllowedAudiences = new List<string> { options.Audience },
+                IssuerSecurityTokenProviders =providersFunc(options)
+            };
+        }
+    }
+}
